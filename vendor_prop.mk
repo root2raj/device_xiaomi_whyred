d05bdbf1f1bde70ff53487ca4e1f9fb5f08b8015
Peiyong Lin <lpy@google.com>
2019-05-03 16:35:02 -0700

    whyred: Turn on GPU protected composition.
    Change-Id: Ie385081961d8b89b81a034178d10176197b639d5

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -143,9 +143,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.havoc.maintainer=root2raj
 
-PRODUCT_PROPERTY_OVERRIDES += \
-    debug.sf.enable_gl_backpressure=1 \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.early_app_phase_offset_ns=500000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000
+# SurfaceFlinger
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.protected_contents=true
